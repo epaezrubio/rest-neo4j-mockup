8f6b5277560632b09700eece8e67567a3dc3bc75
GenMyModel <help@genmymodel.com>
2014-04-24 23:42:14 +0200

    commit by GenMyModel using genmymodel: v20140424_234213

--- src/main/java/jobTest/test/entities/Task.java
+++ src/main/java/jobTest/test/entities/Task.java
@@ -13,9 +13,72 @@ public class Task extends AbstractEntity
 	 * <!-- begin-user-doc -->
 	 * <!--  end-user-doc  -->
 	 * @generated
+	 * @ordered
+	 */
+	
+	public String title;
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!--  end-user-doc  -->
+	 * @generated
+	 * @ordered
+	 */
+	
+	public long date;
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!--  end-user-doc  -->
+	 * @generated
 	 */
 	public Task(){
 		super();
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!--  end-user-doc  -->
+	 * @generated
+	 * @ordered
+	 */
+	
+	public String getTitle() {
+		// TODO : to implement
+		return null;	
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!--  end-user-doc  -->
+	 * @generated
+	 * @ordered
+	 */
+	
+	public long getDate() {
+		// TODO : to implement
+		return 0L;	
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!--  end-user-doc  -->
+	 * @generated
+	 * @ordered
+	 */
+	
+	public void setTitle(String title) {
+		// TODO : to implement	
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!--  end-user-doc  -->
+	 * @generated
+	 * @ordered
+	 */
+	
+	public void setDate(long date) {
+		// TODO : to implement	
 	}
 
 }
