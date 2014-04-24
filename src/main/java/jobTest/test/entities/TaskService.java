8b9fc6b566c84c80901931d5c5dad73b339ee626
GenMyModel <help@genmymodel.com>
2014-04-24 23:41:19 +0200

    commit by GenMyModel using genmymodel: v20140424_234116

---
+++ src/main/java/jobTest/test/entities/TaskService.java
@@ -0,0 +1,86 @@
+package jobTest.test.entities;
+
+
+/**
+ * <!-- begin-user-doc -->
+ * <!--  end-user-doc  -->
+ * @generated
+ */
+
+public class TaskService extends AbstractEntity
+{
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!--  end-user-doc  -->
+	 * @generated
+	 * @ordered
+	 */
+	
+	public String title;
+	
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!--  end-user-doc  -->
+	 * @generated
+	 * @ordered
+	 */
+	
+	public long date;
+	
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!--  end-user-doc  -->
+	 * @generated
+	 */
+	public TaskService(){
+		super();
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!--  end-user-doc  -->
+	 * @generated
+	 * @ordered
+	 */
+	
+	public String getTitle() {
+		// TODO : to implement
+		return null;	
+	}
+	
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!--  end-user-doc  -->
+	 * @generated
+	 * @ordered
+	 */
+	
+	public long getDate() {
+		// TODO : to implement
+		return 0L;	
+	}
+	
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!--  end-user-doc  -->
+	 * @generated
+	 * @ordered
+	 */
+	
+	public void setTitle(String title) {
+		// TODO : to implement	
+	}
+	
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!--  end-user-doc  -->
+	 * @generated
+	 * @ordered
+	 */
+	
+	public void setDate(long date) {
+		// TODO : to implement	
+	}
+	
+}
+
---
+++ src/main/java/jobTest/test/restservices/Task.java
@@ -0,0 +1,22 @@
+package jobTest.test.restservices;
+
+
+/**
+ * <!-- begin-user-doc -->
+ * <!--  end-user-doc  -->
+ * @generated
+ */
+
+public class Task extends AbstractService
+{
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!--  end-user-doc  -->
+	 * @generated
+	 */
+	public Task(){
+		super();
+	}
+
+}
+
