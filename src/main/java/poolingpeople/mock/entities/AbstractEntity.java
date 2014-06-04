e839063cf97265b2ce85284b3a1b7fcb8f65e6e1
alacambra@gmail.com <albert.lacambra@ion2s.com>
2014-06-04 18:17:39 +0200

    Code indented

--- src/main/java/poolingpeople/mock/entities/AbstractEntity.java
+++ src/main/java/poolingpeople/mock/entities/AbstractEntity.java
@@ -12,115 +12,92 @@ import org.neo4j.graphdb.Transaction;
 import poolingpeople.mock.entities.serializers.ISerializer;
 import poolingpeople.mock.entities.serializers.JSONSerializable;
 
+public abstract class AbstractEntity<T> implements JSONSerializable<T> {
 
-public abstract class AbstractEntity<T> implements JSONSerializable<T>
-{
-	
-	@Inject
-	protected org.neo4j.graphdb.GraphDatabaseService dbService;
-	protected org.neo4j.graphdb.Node underlyingNode;
-	protected Long id;
-	
-	protected ISerializer<T> serializer;
+    @Inject
+    protected org.neo4j.graphdb.GraphDatabaseService dbService;
+    protected org.neo4j.graphdb.Node underlyingNode;
+    protected Long id;
 
-	protected AbstractEntity(ISerializer<T> serializer){
-		super();
-		this.serializer = serializer;
-		this.serializer.setSerializableInstance((T) this);
-	}
-        
-        protected AbstractEntity(){
-		super();
-	}
+    protected ISerializer<T> serializer;
 
-	public Long getId() {
-		return id;
-	}
+    protected AbstractEntity(ISerializer<T> serializer) {
+        super();
+        this.serializer = serializer;
+        this.serializer.setSerializableInstance((T) this);
+    }
 
-	public T updateOrCreate() {
-		
-			if (id == null) {
-				Label label = DynamicLabel.label(this.getClass().getSimpleName());
-				underlyingNode = dbService.createNode(label);	
-				id = underlyingNode.getId();
-			}
+    protected AbstractEntity() {
+        super();
+    }
 
-			Field[] fields = this.getClass().getDeclaredFields();
+    public Long getId() {
+        return id;
+    }
 
-			for ( int i = 0; i < fields.length; i++ ){
-				Field field = fields[i];
-				field.setAccessible(true);
-				if ( "id".equals(field.getName()) || "underlyingNode".equals(field.getName()) || "dbService".equals(field.getName())) continue;
-				try {
+    public T updateOrCreate() {
 
-					String key = field.getName();
-					Object value = field.get(this);
+        if (id == null) {
+            Label label = DynamicLabel.label(this.getClass().getSimpleName());
+            underlyingNode = dbService.createNode(label);
+            id = underlyingNode.getId();
+        }
 
-					if( "DELETE".equals(value)){
-						underlyingNode.removeProperty(key);
-					}
+        Field[] fields = this.getClass().getDeclaredFields();
 
-					if ( value != null ){
-						underlyingNode.setProperty(key, value);
-					}
+        for (int i = 0; i < fields.length; i++) {
+            Field field = fields[i];
+            field.setAccessible(true);
+            if ("id".equals(field.getName()) || "underlyingNode".equals(field.getName()) || "dbService".equals(field.getName())) {
+                continue;
+            }
+            try {
 
-				} catch (IllegalArgumentException | IllegalAccessException e) {
-					e.printStackTrace();
-				}
-			}
-		return (T) this;
-	}
+                String key = field.getName();
+                Object value = field.get(this);
 
-	public T load(Long id){
+                if ("DELETE".equals(value)) {
+                    underlyingNode.removeProperty(key);
+                }
 
-		this.id = id;
+                if (value != null) {
+                    underlyingNode.setProperty(key, value);
+                }
 
-		try ( Transaction tx = dbService.beginTx() )
-		{
-			underlyingNode = dbService.getNodeById(id);
-			for(String key : underlyingNode.getPropertyKeys()){
-				try {
-					this.getClass().getDeclaredField(key).setAccessible(true);
-					this.getClass().getDeclaredField(key).set(this, underlyingNode.getProperty(key));
-				} catch (IllegalArgumentException | IllegalAccessException
-						| NoSuchFieldException | SecurityException e) {
-					e.printStackTrace();
-				}
-			}
-			tx.success();
-		}
+            } catch (IllegalArgumentException | IllegalAccessException e) {
+                e.printStackTrace();
+            }
+        }
+        return (T) this;
+    }
 
-		return (T) this;
-	}
+    public T load(Long id) {
 
-	public T setId(@NotNull Long id) {
-		this.id = id;
-		return (T) this;
-	}
-	
-	@Override
-	public ISerializer<T> getSerializer() {
-		return serializer;
-	}
+        this.id = id;
+
+        try (Transaction tx = dbService.beginTx()) {
+            underlyingNode = dbService.getNodeById(id);
+            for (String key : underlyingNode.getPropertyKeys()) {
+                try {
+                    this.getClass().getDeclaredField(key).setAccessible(true);
+                    this.getClass().getDeclaredField(key).set(this, underlyingNode.getProperty(key));
+                } catch (IllegalArgumentException | IllegalAccessException | NoSuchFieldException | SecurityException e) {
+                    e.printStackTrace();
+                }
+            }
+            tx.success();
+        }
+
+        return (T) this;
+    }
+
+    public T setId(@NotNull Long id) {
+        this.id = id;
+        return (T) this;
+    }
+
+    @Override
+    public ISerializer<T> getSerializer() {
+        return serializer;
+    }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
